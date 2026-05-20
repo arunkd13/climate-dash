b1a9e9c8ad4b891c5acf935526def29830b3712f
Arunkumar Dhananjayan <arunkd13@gmail.com>
2026-05-20 11:09:29 +0530

    Updated data

--- docs/data/rainfall.js
+++ docs/data/rainfall.js
@@ -1,7 +1,7 @@
 
 export async function printData(latitude, longitude) {
     const startDate = "2000-01-01";
-    const endDate = "2026-03-01";
+    const endDate = "2026-05-18";
 
     const url = `https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${startDate}&end_date=${endDate}&daily=precipitation_sum&timezone=auto`;
 
